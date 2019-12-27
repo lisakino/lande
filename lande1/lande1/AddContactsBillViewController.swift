68668d532ca06fd670bf37bc50249eb46f3f2c21
lisakino <lisa.kinoshita3@gmail.com>
2019-12-27 14:11:24 -0800

    added addcontactbillviewcontroller

---
+++ lande1/lande1/AddContactsBillViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddContactBillViewController.swift
+//  lande1
+//
+//  Created by Lisa Kinoshita on 2019-12-26.
+//  Copyright © 2019 Lisa Kinoshita. All rights reserved.
+//
+
+import UIKit
+
+class AddContactBillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
