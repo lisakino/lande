c7cad446195e0df2d0aa872e58079013311558f1
lisakino <lisa.kinoshita3@gmail.com>
2019-12-27 10:46:45 -0800

    first commit

---
+++ lande1/lande1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  lande1
+//
+//  Created by Lisa Kinoshita on 2019-12-26.
+//  Copyright © 2019 Lisa Kinoshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
