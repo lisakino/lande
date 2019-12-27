96ddbb14dbfee5db06e7152e012a29a36eba6be6
lisakino <lisa.kinoshita3@gmail.com>
2019-12-27 12:30:29 -0800

    testing stuff out

--- lande1/lande1/ViewController.swift
+++ lande1/lande1/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello Ella", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
