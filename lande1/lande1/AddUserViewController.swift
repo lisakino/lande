ae6318e67adedc6cf04e15a575b5c8da7850720b
Ella Hayashi <ella.j.hayashi@gmail.com>
2019-12-28 16:59:37 -0800

    fixed sigabrt error

--- lande1/lande1/AddUserViewController.swift
+++ lande1/lande1/AddUserViewController.swift
@@ -35,6 +35,10 @@ class AddUserViewController: UIViewController {
         //printing out the contact object
         print("Contact: \(user)")
         
+        
+        
+        
+        
         navigationController?.popViewController(animated: false)
     }
     
