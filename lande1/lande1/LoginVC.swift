afd3079e6fcbc677c99ee207c9ec9468bc50a905
Ella Hayashi <ella.j.hayashi@gmail.com>
2019-12-30 13:17:40 -0800

    added swift file for login and signup vc's

---
+++ lande1/lande1/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  lande1
+//
+//  Created by Lisa Kinoshita on 2019-12-26.
+//  Copyright © 2019 Lisa Kinoshita. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
